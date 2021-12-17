9ea207fbaec562f95c613c39a4d0a734355ca856
KIM DONG IL <dongil.kim@powertask.biz>
2021-12-17 10:01:29 +0900

    toBe, toEqual

--- part-1/index.test.js
+++ part-1/index.test.js
@@ -1,7 +1,18 @@
-const sum = (a, b) => {
-  return a + b;
-};
+const { sum, person } = require("./index");
 
-module.exports = {
-  sum,
-};
+// toBe
+describe("test index.js file => sum", () => {
+  it("sums 1 + 2 to equal 3", () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+});
+
+// toEqual
+describe("test index.js file => person", () => {
+  it("makes a person", () => {
+    expect(person("Patrick", 31)).toEqual({
+      name: "Patrick",
+      age: 31,
+    });
+  });
+});
